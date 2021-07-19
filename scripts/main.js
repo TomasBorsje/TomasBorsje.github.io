62a9695db703b6037ef45297ee31624cd7d03f38
Wingmann <shamanwingman@gmail.com>
2021-07-20 02:49:13 +1200

    Add easter egg
    Adds a dumb easter egg found by clicking on the banner twice, then the button that appears.

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let myVideo = document.querySelector('video');
+let myButton = document.querySelector('button');
+let myImage = document.querySelector("img");
+let clicks = 0;
+
+myButton.onclick = function() {
+	myButton.hidden = true;
+	myVideo.hidden = false;
+	myVideo.play();
+}
+
+myImage.onclick = function() {
+	clicks++;
+	if(clicks>1) {
+		myButton.hidden = false;
+	}
+}
+
+myVideo.onended = (event) => {
+	myVideo.hidden = true;
+	myButton.hidden = true;
+}
